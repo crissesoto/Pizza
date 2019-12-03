72db2c3614b43272e7217fc2d3175f25b92a9a8b
crissesoto <soto.crisse@hotmail.com>
2019-12-03 12:25:37 +0100

    added nav header.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Wardrope Shopping List</title>
-</head>
-<body>
+<?php include"templates/header.php"; ?>
   <h1>Shopping List</h1>
 </body>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Wardrope Shopping List</title>
+</head>
+<body>
+<div class="container-fuid">
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand">Navbar</a>
+    <form class="form-inline">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Add New Item</button>
+    </form>
+  </nav>
+</div>
+
