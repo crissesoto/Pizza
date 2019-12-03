b11e8db179958f3fa9825a7087aaba184cf718a0
crissesoto <soto.crisse@hotmail.com>
2019-12-03 20:39:24 +0100

    showing errors input fields

--- additem.php
+++ additem.php
@@ -1,5 +1,12 @@
 <?php 
 
+//init variables in html iput as value attr
+$name = "";
+$category = "";
+$store = "";
+$link = "";
+
+
 // array to store the errors
 $errors = [
   "name" => "",
@@ -16,25 +23,25 @@ if(isset($_POST["submit"])){
   if(empty($_POST["name"])){
     $errors["name"] = "Name is required";
   }else{
-    echo htmlspecialchars($_POST["name"])."<br>";
+    $name = htmlspecialchars($_POST["name"]);
   };
   // check if name is empty
   if(empty($_POST["category"])){
     $errors["category"] = "Category is required";
   }else{
-    echo htmlspecialchars($_POST["category"])."<br>";
+    $category =  htmlspecialchars($_POST["category"]);
   };
   // check if name is empty
   if(empty($_POST["store"])){
     $errors["store"] = "Store name is required";
   }else{
-    echo htmlspecialchars($_POST["store"])."<br>";
+    $store = htmlspecialchars($_POST["store"]);
   };
   // check if name is empty
   if(empty($_POST["link"])){
     $errors["link"] = "Item link is required";
   }else{
-    echo htmlspecialchars($_POST["link"])."<br>";
+    $link =  htmlspecialchars($_POST["link"]);
   };
 }
 
@@ -51,19 +58,19 @@ if(isset($_POST["submit"])){
   <form action="additem.php" method="POST" class="d-flex flex-column align-self-center">
     
   <label for="name">Your Name:</label>
-    <input type="text" name="name">
+    <input type="text" name="name" value="<?php echo $name ?>">
     <div class="text-danger"> <?php echo $errors["name"]; ?> </div>
 
     <label for="category">Category:</label>
-    <input type="text" name="category">
+    <input type="text" name="category" value="<?php echo $category ?>">
     <div class="text-danger"> <?php echo $errors["category"]; ?> </div>
 
     <label for="store">Store:</label>
-    <input type="text" name="store">
+    <input type="text" name="store" value="<?php echo $store ?>">
     <div class="text-danger"> <?php echo $errors["store"]; ?> </div>
 
     <label for="link">Item link:</label>
-    <input type="href" name="link">
+    <input type="href" name="link" value="<?php echo $link ?>">
     <div class="text-danger"> <?php echo $errors["link"]; ?> </div>
 
     <div class="text-center mt-3">
