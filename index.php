3405e1adda03550518d02de38395de75557cb631
crissesoto <soto.crisse@hotmail.com>
2019-12-05 16:22:31 +0100

    getting data from database

--- index.php
+++ index.php
@@ -9,8 +9,26 @@ $connection = mysqli_connect("localhost","crisse","Serimi03","Shopping_List");
 if(!$connection){
   echo "connection failed: ". mysqli_connect_error();
 }
-?>
 
+// 1: CONSTRUCT QUERY: select all the data from the shopping_items table
+$sqlQuery = "SELECT name, category, store, link FROM shopping_items";
+
+// 2: MAKE QUERY: get the result
+$result = mysqli_query($connection,$sqlQuery);
+
+// 3: FETCH RESULT: get resulting rows as an array
+$items = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// 4: Free up the result after fetching data
+mysqli_free_result($result);
+
+// 5: close the connection
+mysqli_close($connection);
+
+ChromePhp::log($items);
+
+
+?>
 
 
 <?php include "templates/header.php";?>
